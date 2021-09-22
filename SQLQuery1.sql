6145aeab278543224bc8ce4a555a3ab35a16c997
hyunjee4234 <56611411+hyunjee4234@users.noreply.github.com>
2021-09-21 19:19:10 -0700

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,104 @@
+use AdventureWorks2019
+go
+
+--1.
+select productID, Name, Color, ListPrice
+from Production.Product
+
+--2.
+select productID,Name, Color, listprice
+from Production.Product
+where ListPrice = 0
+
+--3.
+select productID, Name, Color, listprice
+from Production.Product
+where Color is Null
+
+--4. 
+select productID, Name, Color, listprice
+from Production.Product
+where Color is not Null
+
+--5.
+select productID, Name, Color, listprice
+from Production.Product
+where Color is not Null and ListPrice > 0
+
+--6
+select 'Name: ' + Name + ' --Color: ' + color as 'Name And Color'
+from Production.Product
+where color is not null
+
+--7
+select 'Name: ' + Name + ' --Color: ' + color as 'Name And Color'
+from Production.Product
+where Name like '%Crankarm%' or Name like '%Chainring%'
+order by  ProductID
+
+--8
+select productID, Name
+from Production.Product
+where ProductID between 400 and 500
+
+--9
+select productID, Name, Color
+from Production.Product
+where Color = 'black' or Color = 'blue'
+
+--10 
+select productID, Name, Color
+from Production.Product
+where Name like 'S%'
+
+--11
+select Name, ListPrice
+from Production.Product
+where Name like 'S%'
+Order by Name
+
+--12
+select Name, ListPrice
+from Production.Product
+where name like 'A%'or name like 'S%'
+order by Name
+
+--13
+select Name, ListPrice
+from Production.Product
+where name like 'SPO[^k]%'
+order by Name
+
+--14
+select Distinct Color
+from Production.Product
+order by 1 DESC
+
+--15
+select Distinct ProductSubcategoryID, Color
+from Production.Product
+where ProductSubcategoryID is not null and Color is not null
+
+--16
+select ProductSubcategoryID, left([name],35) as [name], Color, ListPrice
+from Production.Product
+where color not in ('Red', 'Black')
+    or ListPrice between 1000 and 2000
+	or ProductSubcategoryID = 1
+	order by ProductSubcategoryID, ListPrice
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
